a59d7259fef256b434e58432bbbc93e683c60735
ramilabbaszade <ramil.abbaszade@vabiss.com>
2021-08-09 19:11:49 +0400

    feat: header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
   <div id="nav">
+    <CommonHeader/>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+  import CommonHeader from './components/common/CommonHeader'
+  export default {
+    name:"App",
+    components:{
+      CommonHeader
     }
   }
-}
+
+</script>
+
+<style lang="scss">
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  a{
+    color:black;
+    text-decoration: none;
+  }
 </style>
---
+++ src/components/common/CommonHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav>
+    <div class="nav-brand">
+      veloorent
+    </div>
+    <div class="nav-links">
+      <a href="/about">About</a>
+      <a href="/signup">Signup</a>
+      <a href="/login">Login</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: "CommonHeader",
+}
+
+</script>
+
+<style>
+  nav{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 2rem 3rem;
+    box-shadow: 0px 0px 5px 2px rgba(173, 173, 173, 0.45);
+  }
+  .nav-links{
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+</style>
