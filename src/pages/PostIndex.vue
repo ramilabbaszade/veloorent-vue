212e74ebca1b062b7373f5629b04da9f02fcb3a7
ramilabbaszade <abbasovramil65@gmail.com>
2021-08-12 01:08:37 +0400

    fix(PostIndex): page

--- src/pages/PostIndex.vue
+++ src/pages/PostIndex.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="me">
-    <n-carousel  style="height: 25rem;" show-arrow autoplay>
+    <n-carousel style="height: 25rem;" show-arrow autoplay>
       <img
         class="carousel-img"
         src="https://s.anw.red/fav/1623979004.jpg!/fw/600/quality/77/ignore-error/true"
@@ -18,16 +18,32 @@
         src="https://s.anw.red/news/1623152423.jpg!/both/800x450/quality/78/progressive/true/ignore-error/true"
       />
     </n-carousel>
-    <h1>Velosiped no {{$route.params.id}}</h1>
-    <n-button type="warning">
+    <div>
+      <h1>Velosiped Apolli 745L no {{ $route.params.id }}</h1>
+    </div>
+    <div>BMX | 24 | Sumgayit</div>
+    <div class="post-details__prices">
+      <div class="post-details__prices__item">3azn | saat</div>
+      <div class="post-details__prices__item">10azn | günlük</div>
+      <div class="post-details__prices__item">25azn | aylıq</div>
+    </div>
+    <n-button ghost type="success">
       <template #icon>
         <n-icon>
-          <LocationOnOutlined/>
+          <LocalPhoneOutlined/>
         </n-icon>
       </template>
-      Düzəliş et
+      Əlaqə
     </n-button>
     <hr/>
+    <div class="post-details__author">
+      <n-avatar
+        round
+        size="small"
+        src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"
+      />
+      <div>Ramil Abbas</div>
+    </div>
     <div class="me_user_posts">
       <CommonCard/>
     </div>
@@ -36,15 +52,16 @@
 
 <script>
 import CommonCard from "../components/common/CommonCard";
-import {LocationOnOutlined} from '@vicons/material'
-import {NIcon, NButton, NCarousel} from 'naive-ui'
+import {LocalPhoneOutlined} from '@vicons/material'
+import {NIcon, NButton, NCarousel, NAvatar} from 'naive-ui'
 
 export default {
   name: "PostIndex",
   components: {
     CommonCard,
-    LocationOnOutlined,
+    LocalPhoneOutlined,
     NIcon,
+    NAvatar,
     NCarousel,
     NButton
   }
@@ -59,33 +76,34 @@ export default {
   flex-direction: column;
   align-items: flex-start;
 }
+
 .carousel-img {
   width: 100%;
   height: 100%;
   object-fit: cover;
 }
-.me__picture {
-  width: 18rem;
-  height: 18rem;
-  border-radius: 10px;
-}
-
-.me__user {
+.post-details__author{
   display: flex;
-  align-items: flex-start;
+  align-items: center;
+}
+.post-details__prices {
+  display: flex;
+  justify-content: flex-end;
   width: 100%;
-  gap: 1rem;
-  margin-bottom: 1rem;
-}
 
-.me__info {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
+  &__item {
+    display: flex;
+    flex-direction: column;
+    margin-left: 1rem;
+    background: white;
+    padding: .5rem;
+    border-radius: 5px;
+    box-shadow: #b6b6b6 0px 0px 1px 0px;
+    border: 1px solid #b6b6b6;
 
-  &__location {
-
+    &:hover {
+      box-shadow: #b6b6b6 0px 0px 5px 0px;
+    }
   }
 }
 
