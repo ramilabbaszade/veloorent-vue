03b6ba823b585b26318949e55ee10802354bf6f7
ramilabbaszade <abbasovramil65@gmail.com>
2021-08-16 11:01:11 +0400

    feat: header .scss file created

--- src/components/common/CommonHeader.vue
+++ src/components/common/CommonHeader.vue
@@ -126,26 +126,3 @@ export default {
 
 </script>
 
-<style lang="scss">
-
-nav {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: .5rem 3rem;
-  box-shadow: 0px 0px 5px 2px rgba(173, 173, 173, 0.45);
-}
-
-.nav-links {
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-}
-
-img {
-  width: 5rem;
-  height: 3rem;
-  object-fit: cover;
-}
-</style>
-
