9a667f9aebb5691631d39ea2989125e581af2a07
ramilabbaszade <abbasovramil65@gmail.com>
2021-08-31 16:30:35 +0400

    feat(Add): city field

--- src/pages/PostCreateOrUpdate.vue
+++ src/pages/PostCreateOrUpdate.vue
@@ -3,33 +3,40 @@
     <n-space vertical>
       <div>
         <div>Başlıq</div>
-        <n-input v-model:value="value" type="text" placeholder="Basic Input"/>
+        <n-input v-model:value="value" type="text" placeholder="Daxil edin"/>
       </div>
       <div>
         <div>Qeyd:</div>
-        <n-input v-model:value="value" type="textarea" placeholder="Basic Textarea"/>
+        <n-input v-model:value="value" type="textarea" placeholder="Daxil edin"/>
       </div>
       <n-space vertical>
-
-      <div>
-        <div>Ölçü:</div>
-        <n-select v-model:value="value" size="medium" :options="sizeOptions"/>
-      </div>
-      <div>
-        <div>Tip:</div>
-        <n-select v-model:value="value" size="medium" :options="typeOptions"/>
-      </div>
+        <div>
+          <div>Ölçü:</div>
+          <n-select v-model:value="value" size="medium" :options="sizeOptions" placeholder="Daxil edin"/>
+        </div>
+        <div>
+          <div>Tip:</div>
+          <n-select v-model:value="value" size="medium" :options="typeOptions" placeholder="Daxil edin"/>
+        </div>
+        <div>
+          <div>Şəhər:</div>
+          <n-auto-complete :options="cityOptions" v-model:value="value" placeholder="Daxil edin"/>
+        </div>
       </n-space>
     </n-space>
+    <n-button type="primary">Primary</n-button>
+
   </div>
 </template>
 <script>
-import {NInput, NSpace, NSelect} from 'naive-ui'
+import {NInput, NSpace, NSelect, NAutoComplete, NButton} from 'naive-ui'
 
 export default {
   name: "PostCreateOrUpdate",
   components: {
     NInput,
+    NButton,
+    NAutoComplete,
     NSpace,
     NSelect
   },
@@ -90,6 +97,9 @@ export default {
           value: "electric",
           label: "ELektrik"
         },
+      ],
+      cityOptions: [
+        "Baki", "Sumgayit"
       ]
     }
   }
